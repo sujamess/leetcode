445647a3fc87bd6f6c7a9cc3356f6ba271fa2c98
sujamess <surarerk.boonjit@gmail.com>
2023-02-07 16:31:37 +0700

    feat: improve solution using memoizer

--- 494_target_sum/solution.go
+++ 494_target_sum/solution.go
@@ -1,10 +1,19 @@
 package targetsum
 
-func Solution(nums []int, target int) int {
-	return dp(nums, 0, 0, target)
+type Memoizer struct {
+	Index int
+	Sum   int
 }
 
-func dp(nums []int, startIndex, accumulate, target int) int {
+func Solution(nums []int, target int) int {
+	return dp(nums, 0, 0, target, make(map[Memoizer]int, 0))
+}
+
+func dp(nums []int, startIndex, accumulate, target int, memoizer map[Memoizer]int) int {
+	if v, ok := memoizer[Memoizer{Index: startIndex, Sum: accumulate}]; ok {
+		return v
+	}
+
 	if startIndex == len(nums) {
 		if accumulate == target {
 			return 1
@@ -13,7 +22,9 @@ func dp(nums []int, startIndex, accumulate, target int) int {
 	}
 
 	ways := 0
-	ways += dp(nums, startIndex+1, accumulate+nums[startIndex], target)
-	ways += dp(nums, startIndex+1, accumulate-nums[startIndex], target)
+	ways += dp(nums, startIndex+1, accumulate+nums[startIndex], target, memoizer)
+	ways += dp(nums, startIndex+1, accumulate-nums[startIndex], target, memoizer)
+
+	memoizer[Memoizer{Index: startIndex, Sum: accumulate}] = ways
 	return ways
 }
